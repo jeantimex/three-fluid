b56646c0d0abf57c34b1b89f119e248b55dd69c4
Yong Su <jean.timex@gmail.com>
2025-05-14 22:19:09 -0700

    Added the ocean sample

---
+++ src/ocean/index.ts
@@ -0,0 +1,75 @@
+import * as THREE from "three";
+
+// init camera, scene, renderer
+const scene: THREE.Scene = new THREE.Scene();
+const fov = 75;
+const aspect = window.innerWidth / window.innerHeight;
+const camera = new THREE.PerspectiveCamera(fov, aspect, 0.1, 1000);
+camera.position.z = 100;
+camera.lookAt(scene.position);
+const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(0xc4c4c4);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+const clock = new THREE.Clock();
+
+const tuniform = {
+  iGlobalTime: {
+    type: "f",
+    value: 0.1,
+  },
+  iResolution: {
+    type: "v2",
+    value: new THREE.Vector2(),
+  },
+  iMouse: {
+    type: "v4",
+    value: new THREE.Vector4(),
+  },
+};
+
+// Mouse position in - 1 to 1
+renderer.domElement.addEventListener("mousedown", function (e: MouseEvent) {
+  var canvas = renderer.domElement;
+  var rect = canvas.getBoundingClientRect();
+  tuniform.iMouse.value.x =
+    ((e.clientX - rect.left) / window.innerWidth) * 2 - 1;
+  tuniform.iMouse.value.y =
+    ((e.clientY - rect.top) / window.innerHeight) * -2 + 1;
+});
+renderer.domElement.addEventListener("mouseup", function (e: MouseEvent) {
+  var canvas = renderer.domElement;
+  var rect = canvas.getBoundingClientRect();
+  tuniform.iMouse.value.z =
+    ((e.clientX - rect.left) / window.innerWidth) * 2 - 1;
+  tuniform.iMouse.value.w =
+    ((e.clientY - rect.top) / window.innerHeight) * -2 + 1;
+});
+// resize canvas function
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+tuniform.iResolution.value.x = window.innerWidth;
+tuniform.iResolution.value.y = window.innerHeight;
+// Create Plane
+var material = new THREE.ShaderMaterial({
+  uniforms: tuniform,
+  vertexShader: document.getElementById("vertex-shader")!.textContent!,
+  fragmentShader: document.getElementById("fragment-shader")!.textContent!,
+});
+var mesh = new THREE.Mesh(
+  new THREE.PlaneGeometry(window.innerWidth, window.innerHeight, 40),
+  material
+);
+scene.add(mesh);
+
+// draw animation
+function render() {
+  tuniform.iGlobalTime.value += clock.getDelta();
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+render();
