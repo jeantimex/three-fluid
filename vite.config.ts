8ff4379e9862fc984ef7a08b510b7e7843704e2e
Yong Su <jean.timex@gmail.com>
2025-05-14 23:29:28 -0700

    support deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import vitePluginString from 'vite-plugin-string';
 
+// Get the repository name from package.json or environment
+const repoName = 'three-fluid';
+
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
   build: {
     outDir: 'dist',
   },
